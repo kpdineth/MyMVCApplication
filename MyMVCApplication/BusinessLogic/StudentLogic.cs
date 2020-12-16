3d5b0802110314ac0edcaa4601e69be4f73472fe
dinethp <dinethp@NA-COM-084>
2020-12-16 17:03:09 +0800

    16/12/5/02

---
+++ MyMVCApplication/BusinessLogic/StudentLogic.cs
@@ -0,0 +1,19 @@
+﻿using MyMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MyMVCApplication.BusinessLogic
+{
+    public class StudentLogic
+    {
+        EmEntities Student = new EmEntities();
+    }
+
+    public List<TestTable> GetStudentRoles()
+    {
+        var std = Student.
+    }
+}
